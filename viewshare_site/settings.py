2adcd24deea90d7cc0d0da0e57f6444c3c94693b
David Feeney <dfeeney@gmail.com>
2013-09-09 14:17:06 -0400

    removing obsolete context processors

--- viewshare_site/settings.py
+++ viewshare_site/settings.py
@@ -96,7 +96,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "viewshare.utilities.context_processors.viewshare_settings",
     "viewshare.apps.vendor.notification.context_processors.notification",
-    "viewshare.apps.vendor.announcements.context_processors.site_wide_announcements",
     "viewshare.apps.account.context_processors.account",
     "viewshare.apps.connections.context_processors.invitations",
     )
